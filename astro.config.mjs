ccf9ff97921105bea1262ea05d0e1577fc253526
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2023-12-06 23:18:03 -0300

    Enable compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
+import compressor from 'astro-compressor'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://thiagomajesk.github.io/',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap(), tailwind(), compressor()],
 	markdown: {
 		shikiConfig: {
 			wrap: true,
