14c27dd6fdc1d139317f37749fd391053f80b3fe
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2023-12-06 23:41:34 -0300

    Run prettier

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ export const SITE_DESCRIPTION = 'Welcome to my website!'
 export const GITHUB_URL = 'https://github.com/thiagomajesk'
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/thiago-majesk-goulart'
 
-export const DEFAULT_PAGE_SIZE = 50;
+export const DEFAULT_PAGE_SIZE = 50
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,17 @@ import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
-  const posts = await getCollection('posts')
+	const posts = await getCollection('posts')
 
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      link: `/posts/${post.slug}/`,
-      pubDate: post.data.date,
-      categories: post.data.tags,
-    }))
-  })
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			link: `/posts/${post.slug}/`,
+			pubDate: post.data.date,
+			categories: post.data.tags
+		}))
+	})
 }
