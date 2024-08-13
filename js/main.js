133e3ca9e3d81f99ecd5a168721a5edff58107ec
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2024-08-13 20:31:53 -0300

    Add scroll indicator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 import "unpoly";
+import ScrollProgress from "scroll-progress-indicator";
+
+up.on("up:fragment:inserted", () => {
+  ScrollProgress.init({
+    color: "#50FA7B",
+    height: "4px",
+    position: "bottom",
+  });
+});
